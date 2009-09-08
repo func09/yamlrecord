982d367c2dce3ab65cb8a3208bc3cf9a7d3a4468
func09 <mitsuru.haga@gmail.com>
2009-09-09 00:53:07 +0900

    YAMLファイルをひとつにして、RAILS_ENVを内包するようにした
    ex) db/yamlrecord/hoge.yaml
    :development
      value1: 1
      value2: 2
    :production
      value1: a
      value2: b

--- lib/yamlrecord.rb
+++ lib/yamlrecord.rb
@@ -2,27 +2,34 @@ require 'yaml/store'
 
 class YAMLRecord
 
-  @@yaml_fields = []
-  @@db_file = "#{RAILS_ROOT}/db/yamlrecord/#{self.to_s.underscore}_#{RAILS_ENV}.yml"
-  @@yaml_db = YAML::Store.new @@db_file
+  def self.inherited(subclass)
+    subclass.instance_eval %-
+      @yaml_fields = []
+      @db_file = "#{RAILS_ROOT}/db/yamlrecord/#{subclass.to_s.underscore}.yml"
+      @yaml_db = YAML::Store.new @db_file
+    -
+  end
 
   def self.field_names
-    @@yaml_fields
+    @yaml_fields
   end
   
   def self.save
-    @@yaml_db.transaction do
-      @@yaml_fields.each do |yaml_field|
-        @@yaml_db[yaml_field] = instance_variable_get("@inheritable_attributes")[yaml_field.to_sym]
+    @yaml_db.transaction do
+      @yaml_db[RAILS_ENV] ||= {}
+      @yaml_fields.each do |yaml_field|
+        @yaml_db[RAILS_ENV][yaml_field] = method("#{yaml_field}").call
       end
     end
   end
 
   def self.reload
-    @@yaml_db.transaction do
-      @@yaml_fields.each do |yaml_field|
-        if respond_to?("#{yaml_field}=")
-          method("#{yaml_field}=").call(@@yaml_db[yaml_field])
+    @yaml_db.transaction do
+      if @yaml_db[RAILS_ENV]
+        @yaml_fields.each do |yaml_field|
+          if respond_to?("#{yaml_field}=") && @yaml_db[RAILS_ENV][yaml_field]
+            method("#{yaml_field}=").call(@yaml_db[RAILS_ENV][yaml_field])
+          end
         end
       end
     end
@@ -30,10 +37,10 @@ class YAMLRecord
   
   def self.attr_yaml_field(*fields)
     fields.each do |field|
-      unless @@yaml_fields.include?(field.to_s)
+      unless @yaml_fields.include?(field.to_s)
         class_inheritable_accessor field
-        (@@yaml_fields ||= []) << field.to_s
-        @@yaml_fields.uniq!
+        (@yaml_fields ||= []) << field.to_s
+        @yaml_fields.uniq!
       end
     end
     reload
